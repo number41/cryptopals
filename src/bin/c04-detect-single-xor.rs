0752062604bcc51fceef5e9de2d40429912a5719
Sean Peterson <wipeters@gmail.com>
2016-08-28 18:28:59 -0400

    [c04] Complete challenge
    Find the highest scoring decrypted plaintext of multiple lines

--- src/bin/c04-detect-single-xor.rs
+++ src/bin/c04-detect-single-xor.rs
@@ -5,15 +5,45 @@ use std::io::BufReader;
 use std::fs::File;
 use std::io::prelude::*;
 
+use cryptopals::utils::decode_hex;
+use cryptopals::utils::reverse_xor;
 
-fn iterate_file(f: File) -> io::Result<()> {
+struct Candidate {
+    score: f32,
+    original: String,
+    plaintext: String,
+}
+
+fn iterate_file(f: File) -> Option<Candidate> {
     let reader = BufReader::new(f);
+    let mut candidate: Option<Candidate> = None;
+    let mut max_score = 0.0;
 
     for line in reader.lines() {
-        println!("{}", try!(line));
+        let hex_text = match line {
+            Ok(t) => t,
+            Err(e) => panic!("Error?!! {}", e)
+        };
+
+        // 1) Convert line -> bytes
+        let input_bytes = decode_hex(&hex_text); 
+
+        // 2) Determine best 'decryption' (decrypted text, score)
+        match reverse_xor(&input_bytes) {
+            Some(c) => {
+                println!("Possibility: {}", c.plaintext);
+                if c.score > max_score {
+                    candidate = Some(Candidate {score: c.score, original: hex_text, plaintext: c.plaintext});
+                    max_score = c.score;
+                }
+            },
+            None => {
+                //println!("Nothing good was found?");
+            }
+        }
     }
 
-    Ok(())
+    return candidate;
 }
 
 fn main() {
@@ -27,10 +57,14 @@ fn main() {
 
     println!("Opened the file!!!!");
     match iterate_file(f) {
-        Err(e) => {
-            println!("Error: {}", e);
+        Some(c) => {
+            println!("Possible best match");
+            println!("   Score:    {}", c.score);
+            println!("   Original: {}", c.original);
+            println!("   Plain:    {}", c.plaintext);
         },
-        Ok(_) => {
+        None => {
+            println!("Nothing was found?");
         }
     }
 }
