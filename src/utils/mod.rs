8933ea8360a00e6d39ab60318d72936b935d2390
Sean Peterson <wipeters@gmail.com>
2016-08-27 20:00:49 -0400

    [c04] Factor out c03 functions for reuse

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -54,3 +54,58 @@ pub fn xor_buffers(lhs: &[u8], rhs: &[u8]) -> Vec<u8> {
     assert!(lhs.len() == rhs.len());
     lhs.iter().zip(rhs.iter()).map(|(l,r)| l ^ r).collect()
 }
+
+pub fn score_bytes(input: &[u8]) -> f32 {
+    let mut progress = 0.0;
+    for byte in input {
+        if !(32 <= *byte && *byte <= 127) {
+            return -1.0
+        }
+        progress += get_frequency(*byte as char)
+    }
+    return progress;
+}
+
+fn get_frequency(c: char) -> f32 {
+    let upper = c.to_uppercase().collect::<String>();
+    match upper.as_ref() {
+        "A" => 8.2,
+        "B" => 1.5,
+        "C" => 2.8,
+        "D" => 4.3,
+        "E" => 12.7,
+        "F" => 2.2,
+        "G" => 2.0,
+        "H" => 6.1,
+        "I" => 7.0,
+        "J" => 0.2,
+        "K" => 0.8,
+        "L" => 4.0,
+        "M" => 2.4,
+        "N" => 6.7,
+        "O" => 7.5,
+        "P" => 1.9,
+        "Q" => 0.1,
+        "R" => 6.0,
+        "S" => 6.3,
+        "T" => 9.1,
+        "U" => 2.8,
+        "V" => 1.0,
+        "W" => 2.4,
+        "X" => 0.2,
+        "Y" => 2.0,
+        "Z" => 0.1,
+        "'" => 0.05,
+        " " => 10.0,
+        "." => 0.2,
+        "!" => 0.05,
+        "?" => 0.05,
+        "\"" => 0.05,
+        _ => 0.0,
+    }
+}
+
+pub fn single_xor(key: u8, bytes: &[u8]) -> Vec<u8> {
+    bytes.iter().map(|byte| key ^ byte).collect()
+}
+
